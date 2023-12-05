07bb421537cb3aedaac349cc183984ba57c06f0e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-05 11:58:34 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/stripe/payment_manager.dart
+++ lib/backend/stripe/payment_manager.dart
@@ -11,7 +11,7 @@ import '../../flutter_flow/flutter_flow_util.dart';
 import '../../flutter_flow/flutter_flow_widgets.dart';
 import '../cloud_functions/cloud_functions.dart';
 
-final _isProd = true;
+final _isProd = false;
 
 // Stripe Credentials
 const _kProdStripePublishableKey =
