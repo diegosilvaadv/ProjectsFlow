2a32e3ac5758e021e7ac59930e58adb049502bb1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-05 16:52:01 +0000

    Updating to latest FlutterFlow output.

--- lib/componts/video_view/video_view_widget.dart
+++ lib/componts/video_view/video_view_widget.dart
@@ -50,76 +50,83 @@ class _VideoViewWidgetState extends State<VideoViewWidget> {
   Widget build(BuildContext context) {
     context.watch<FFAppState>();
 
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 20.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FlutterFlowIconButton(
-                borderColor: FlutterFlowTheme.of(context).secondaryBackground,
-                borderRadius: 20.0,
-                borderWidth: 1.0,
-                buttonSize: 50.0,
-                fillColor: FlutterFlowTheme.of(context).primaryBackground,
-                icon: Icon(
-                  Icons.close,
-                  color: FlutterFlowTheme.of(context).primaryText,
-                  size: 30.0,
-                ),
-                showLoadingIndicator: true,
-                onPressed: () async {
-                  logFirebaseEvent('VIDEO_VIEW_COMP_close_ICN_ON_TAP');
-                  logFirebaseEvent('IconButton_bottom_sheet');
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-        Align(
-          alignment: AlignmentDirectional(0.00, 0.00),
-          child: Material(
-            color: Colors.transparent,
-            elevation: 5.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            child: Container(
-              width: 1280.0,
-              height: 720.0,
-              decoration: BoxDecoration(
-                color: FlutterFlowTheme.of(context).primaryBackground,
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 4.0,
-                    color: FlutterFlowTheme.of(context).accent1,
-                    offset: Offset(0.0, 2.0),
-                  )
+    return Align(
+      alignment: AlignmentDirectional(0.00, 0.00),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Align(
+            alignment: AlignmentDirectional(0.00, 0.00),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 20.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlutterFlowIconButton(
+                    borderColor:
+                        FlutterFlowTheme.of(context).secondaryBackground,
+                    borderRadius: 20.0,
+                    borderWidth: 1.0,
+                    buttonSize: 50.0,
+                    fillColor: FlutterFlowTheme.of(context).primaryBackground,
+                    icon: Icon(
+                      Icons.close,
+                      color: FlutterFlowTheme.of(context).primaryText,
+                      size: 30.0,
+                    ),
+                    showLoadingIndicator: true,
+                    onPressed: () async {
+                      logFirebaseEvent('VIDEO_VIEW_COMP_close_ICN_ON_TAP');
+                      logFirebaseEvent('IconButton_bottom_sheet');
+                      Navigator.pop(context);
+                    },
+                  ),
                 ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: AlignmentDirectional(0.00, 0.00),
+            child: Material(
+              color: Colors.transparent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12.0),
               ),
-              alignment: AlignmentDirectional(0.00, 0.00),
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(20.0, 20.0, 20.0, 20.0),
-                child: FlutterFlowVideoPlayer(
-                  path:
-                      'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4',
-                  videoType: VideoType.network,
-                  autoPlay: false,
-                  looping: true,
-                  showControls: true,
-                  allowFullScreen: true,
-                  allowPlaybackSpeedMenu: false,
+              child: Container(
+                width: 1280.0,
+                height: 720.0,
+                decoration: BoxDecoration(
+                  color: FlutterFlowTheme.of(context).primaryBackground,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4.0,
+                      color: FlutterFlowTheme.of(context).accent1,
+                      offset: Offset(0.0, 2.0),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                alignment: AlignmentDirectional(0.00, 0.00),
+                child: Padding(
+                  padding:
+                      EdgeInsetsDirectional.fromSTEB(20.0, 20.0, 20.0, 20.0),
+                  child: FlutterFlowVideoPlayer(
+                    path: widget.video!,
+                    videoType: VideoType.network,
+                    autoPlay: false,
+                    looping: true,
+                    showControls: true,
+                    allowFullScreen: true,
+                    allowPlaybackSpeedMenu: false,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
